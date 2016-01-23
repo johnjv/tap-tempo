d1b1a7ece6c38e4926c02e594167e0792752ece5
Justin Varghese John <justinjohn107@gmail.com>
2016-01-22 23:36:06 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+This is the source code to a tap tempo web application. It is a simple responsive page built using the [Web Starter Kit](https://developers.google.com/web/starter-kit/) with [Gulp](http://gulpjs.com/) as the build system.
+
+## Prerequisites
+
+### [Node.js](https://nodejs.org)
+
+Bring up a terminal and type `node --version`.
+Node should respond with a version at or above 0.10.x.
+If you require Node, go to [nodejs.org](https://nodejs.org) and click on the big green Install button.
+
+### [Gulp](http://gulpjs.com)
+
+Bring up a terminal and type `gulp --version`.
+If Gulp is installed it should return a version number at or above 3.9.x.
+If you need to install/upgrade Gulp, open up a terminal and type in the following:
+
+```sh
+$ npm install --global gulp
+```
+
+*This will install Gulp globally. Depending on your user account, you may need to [configure your system](https://github.com/sindresorhus/guides/blob/master/npm-global-without-sudo.md) to install packages globally without administrative privileges.*
+
+
+# How to Run
+
+Clone repo and run
+
+```sh
+$ npm install
+```
+
+Serve up a local version of the site with gulp by running
+
+```sh
+$ gulp serve
+```
---
+++ app/scripts/main.js
@@ -0,0 +1,134 @@
+/*!
+ *
+ *  justinvarghesejohn.com
+ *  Copyright 2015 Justin Varghese John
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+
+'use strict';
+
+var TapTempo = (function() {
+	function TapTempo() {
+		this.taps = [];
+		this.isRecording = false;
+	}
+
+	TapTempo.prototype.recordTime = function() {
+		var date = new Date();
+		var time = date.getTime();
+		console.log(time);
+		this.taps.push(time);
+		this.isRecording = true;
+	};
+
+	TapTempo.prototype.getBpm = function() {
+		var deltas = this.getDeltas();
+		return this.calculateBpm(deltas);
+	};
+
+	TapTempo.prototype.clearTimes = function() {
+		this.taps = [];
+		this.isRecording = false;
+	};
+
+	TapTempo.prototype.getDeltas = function() {
+		var deltas = [];
+		for(var i = 0; i < this.taps.length - 1; i++) {
+			var delta = this.taps[i+1] - this.taps[i];
+			deltas.push(delta);
+		}
+		return deltas;
+	};
+
+	TapTempo.prototype.calculateBpm = function(deltas) {
+		var sum = 0;
+		var average;
+		for(var delta in deltas) {
+			sum += deltas[delta];
+		}
+		average = sum / deltas.length;
+
+		return (60000 / average);
+	};
+
+	TapTempo.prototype.isRecording = function() {
+		return this.isRecording;
+	};
+
+	return TapTempo;
+
+})();
+
+
+var timer = {
+  reset: function() {
+  	console.log('reset');
+    this.stop();
+    this.start();
+  },
+
+  start: function() {
+    this.stop();
+    var self = this;
+    this.timeoutID = window.setTimeout( function() {
+    	self.stop();
+    	tapTempo.clearTimes();
+			$('.mdl-layout__content').removeClass('animate-calculate');
+			$('.mdl-layout__content').addClass('animate-reset');
+			$('#bpm').text('---');
+    }, 3000);
+  },
+
+  stop: function() {
+    if(typeof this.timeoutID == "number") {
+      window.clearTimeout(this.timeoutID);
+      delete this.timeoutID;
+    }
+  }
+};
+
+document.querySelector('.tapper').addEventListener('touchstart', handleTap ,false);
+document.querySelector('.tapper').addEventListener('click', handleTap ,false);
+
+var tapTempo = new TapTempo();
+
+function handleTap(event) {
+	event.preventDefault();
+	console.log(event);
+	tapTempo.recordTime();
+	timer.reset();
+	updateView();
+}
+
+function updateView() {
+	if(tapTempo.taps.length > 2) {
+		var bpm = Math.round(tapTempo.getBpm());
+		$('#bpm').text(bpm);
+	}
+	else if(tapTempo.taps.length == 0) {
+		$('#bpm').text('---');
+	}
+	else if(tapTempo.taps.length == 1) {
+		$('.mdl-layout__content').removeClass('animate-reset');
+		$('.mdl-layout__content').addClass('animate-calculate');
+	}
+	// else {
+	// 	$('#bpm').text('tap again');
+	// }
+}
+
+function visualizeTap() {
+
+}
