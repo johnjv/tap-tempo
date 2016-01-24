540d21c465e3f1a260f60692374960387080cdc0
Justin Varghese John <justinjohn107@gmail.com>
2016-01-24 12:51:18 -0600

    Add copyright

--- app/scripts/workers/metroworker.js
+++ app/scripts/workers/metroworker.js
@@ -1,3 +1,24 @@
+/*!
+ *
+ *  Tap Tempo
+ *  Copyright 2015 Justin Varghese John
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+
+'use strict';
+
 var timerID=null;
 var interval=100;
 
@@ -22,4 +43,4 @@ self.onmessage=function(e){
 	}
 };
 
-postMessage('hi there');
+postMessage('metronome initialized');
