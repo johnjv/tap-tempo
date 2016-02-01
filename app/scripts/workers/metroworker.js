9bca6b87387008deb0befbbc40878cd7455d9df9
Justin Varghese John <justinjohn107@gmail.com>
2016-02-01 17:47:49 -0600

    Chore fixing eslint issues

--- app/scripts/workers/metroworker.js
+++ app/scripts/workers/metroworker.js
@@ -19,25 +19,24 @@
 
 'use strict';
 
-var timerID=null;
-var interval=100;
+var timerID = null;
+var interval = 100;
 
-self.onmessage=function(e){
-	if (e.data=="start") {
-		console.log("starting");
-		timerID=setInterval(function(){postMessage("tick");},interval)
+self.onmessage = function(e) {
+	if (e.data === 'start') {
+		timerID = setInterval( () => {postMessage('tick');}, interval);
 	}
 	else if (e.data.interval) {
-		console.log("setting interval");
-		interval=e.data.interval;
-		console.log("interval="+interval);
+		console.log('setting interval');
+		interval = e.data.interval;
+		console.log('interval = ' + interval);
 		if (timerID) {
 			clearInterval(timerID);
-			timerID=setInterval(function(){postMessage("tick");},interval)
+			timerID = setInterval( () => {postMessage('tick');}, interval);
 		}
 	}
-	else if (e.data=="stop") {
-		console.log("stopping");
+	else if (e.data === 'stop') {
+		console.log('stopping');
 		clearInterval(timerID);
 		timerID=null;
 	}
